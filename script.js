75922b68a1a6311c7247a4497592810154f0ffbc
tinik38 <tinik38@gmail.com>
2026-08-28 19:40:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,20 @@ function loadTimings() {
 
   const url = `https://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}`;
 
-  document.getElementById("vakitler").innerHTML = "Yükleniyor...";
-
   fetch(url)
     .then(response => response.json())
     .then(data => {
       const t = data.data.timings;
-      document.getElementById("vakitler").innerHTML = `
-        <strong>İmsak:</strong> ${t.Imsak}<br>
-        <strong>Güneş:</strong> ${t.Sunrise}<br>
-        <strong>Öğle:</strong> ${t.Dhuhr}<br>
-        <strong>İkindi:</strong> ${t.Asr}<br>
-        <strong>Akşam:</strong> ${t.Maghrib}<br>
-        <strong>Yatsı:</strong> ${t.Isha}
-      `;
+
+      // Yeni kutulara yaz
+      document.getElementById("imsak").innerText = t.Imsak;
+      document.getElementById("gunes").innerText = t.Sunrise;
+      document.getElementById("ogle").innerText = t.Dhuhr;
+      document.getElementById("ikindi").innerText = t.Asr;
+      document.getElementById("aksam").innerText = t.Maghrib;
+      document.getElementById("yatsi").innerText = t.Isha;
     })
     .catch(err => {
-      document.getElementById("vakitler").innerHTML =
-        "Bir hata oluştu, daha sonra tekrar dene.";
       console.error(err);
     });
 }
